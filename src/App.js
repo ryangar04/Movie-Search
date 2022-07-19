231e7f20b8832a355b76ddc6e095967026138887
Ryan Garcia <104345545+ryangar04@users.noreply.github.com>
2022-07-19 16:10:22 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import SearchMovies from './components/SearchMovie';
+
+export default function App() {
+  return (
+    <div className="container">
+      <h1 className="title">React Movie Search</h1>
+      <SearchMovies/>
+    </div>
+  );
+}
